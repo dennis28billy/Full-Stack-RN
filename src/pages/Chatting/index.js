08a113dadb0cb69dfc4a7d901560c3d035025cb7
unknown <s11810068@student.unklab.ac.id>
2021-05-24 21:09:58 +0800

    slicing halaman Chatting(Header & Text Chat Date)

--- src/pages/Chatting/index.js
+++ src/pages/Chatting/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {ChatItem, Header, InputChat} from '../../components';
+import {colors, fonts} from '../../utils';
 
 const Chatting = () => {
   return (
     <View>
-      <Header title="Nairobi Putri Hayza" />
-      <Text>Senin, 21 Maret, 2020</Text>
+      <Header type="dark-profile" title="Nairobi Putri Hayza" />
+      <Text style={styles.chatDate}>Senin, 21 Maret, 2020</Text>
       <ChatItem />
       <ChatItem />
       <ChatItem />
@@ -17,4 +18,12 @@ const Chatting = () => {
 
 export default Chatting;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  chatDate: {
+    fontSize: 11,
+    fontFamily: fonts.primary.normal,
+    color: colors.text.secondary,
+    marginVertical: 20,
+    textAlign: 'center',
+  },
+});
