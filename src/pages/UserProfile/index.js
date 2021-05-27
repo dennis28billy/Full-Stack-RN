b91b20f5e60f249dade9c66055b20f6535842557
unknown <s11810068@student.unklab.ac.id>
2021-05-27 21:41:59 +0800

    menmmabahkan fungsi signOut pada halaman UserProfile

--- src/pages/UserProfile/index.js
+++ src/pages/UserProfile/index.js
@@ -2,7 +2,9 @@ import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {ILNullPhoto} from '../../assets';
 import {Gap, Header, List, Profile} from '../../components';
-import {getData} from '../../utils';
+import {getData, colors} from '../../utils';
+import {firebase} from '../../config';
+import {showMessage} from 'react-native-flash-message';
 
 const UserProfile = ({navigation}) => {
   const [profile, setProfile] = useState({
@@ -17,6 +19,24 @@ const UserProfile = ({navigation}) => {
       setProfile(data);
     });
   }, []);
+
+  const signOut = () => {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        console.log('success sign out');
+        navigation.replace('GetStarted');
+      })
+      .catch(error => {
+        showMessage({
+          message: error.message,
+          type: 'default',
+          backgroundColor: colors.error,
+          color: colors.white,
+        });
+      });
+  };
   return (
     <View style={styles.page}>
       <Header title="Profile" onPress={() => navigation.goBack()} />
@@ -50,10 +70,11 @@ const UserProfile = ({navigation}) => {
         icon="rate"
       />
       <List
-        name="Help Center"
-        desc="Read our guidelines"
+        name="Sign Out"
+        desc="Sign out this profile"
         type="next"
         icon="help"
+        onPress={signOut}
       />
     </View>
   );
