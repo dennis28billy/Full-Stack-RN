587a8b648f0742ca2cbff422eb10ba215eefdb00
unknown <s11810068@student.unklab.ac.id>
2021-05-27 22:45:18 +0800

    install redux & react-redux untuk implementasi global state management

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import {NavigationContainer} from '@react-navigation/native';
-import React from 'react';
-import {StyleSheet} from 'react-native';
+import React, {useState} from 'react';
 import Router from './router';
 import FlashMessage from 'react-native-flash-message';
+import {Loading} from './components';
+import {Provider} from 'react-redux';
+import store from './redux/store';
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
   return (
-    <>
+    <Provider store={store}>
       <NavigationContainer>
         <Router />
       </NavigationContainer>
       <FlashMessage position="top" />
-    </>
+      {loading && <Loading />}
+    </Provider>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
